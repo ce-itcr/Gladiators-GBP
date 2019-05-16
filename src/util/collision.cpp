a4d038129507b61cd6dd418197d84f9ff90f0846
Agustin <joseagustinveneveg@gmail.com>
2019-05-15 22:13:22 -0600

    Collision Update
    Collision Update

--- src/util/collision.cpp
+++ src/util/collision.cpp
@@ -1,8 +1,10 @@
 #include "collision.h"
+#include <iostream>
+using namespace std;
 
 bool Collision::collide(QRect rect1, QRect rect2)
 {
-    return rect1.contains(rect2);
+    return rect1.intersects(rect2);
 }
 
 bool Collision::collide(QRect rect, QList<QRect> rects)
