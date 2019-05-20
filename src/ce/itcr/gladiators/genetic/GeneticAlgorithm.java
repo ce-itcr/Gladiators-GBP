a4b83b90acbca951b1e5d55d6844038faec3fa46
Iván Solís Ávila <isolis@solavcr.com>
2019-05-20 16:39:23 -0600

    Final touches

--- src/ce/itcr/gladiators/genetic/GeneticAlgorithm.java
+++ src/ce/itcr/gladiators/genetic/GeneticAlgorithm.java
@@ -15,13 +15,8 @@ public class GeneticAlgorithm {
 	public void newGeneration(Collection<GladiatorSend> gladiatorCollection, int numOfGladiatorsToGenerate) {
 		Gladiators.getInstance().printGladiatorPopulation();
 		Gladiators.getInstance().population = collectionToGladiatorArray(gladiatorCollection);
-//		System.out.println("population[0]: " + Gladiators.getInstance().population[0]);
-//		System.out.println("population[58]: " + Gladiators.getInstance().population[58]);
 		Gladiators.getInstance().sortGladiatorPopulation();
 		for (int i = 0; i < numOfGladiatorsToGenerate; i++) {
-//			System.out.println("population size: " + Gladiators.getInstance().getPopulationSize());
-//			System.out.println("random: " + Gladiators.getInstance().population[0]);
-//			System.out.println("Fittest: " + Gladiators.getInstance().getFittestGladiator());
 			Gladiator newGladiator = Gladiators.getInstance().getFittestGladiator().crossover(Gladiators.getInstance().population[random.nextInt(Gladiators.getInstance().getPopulationSize()/3)]);
 			Gladiators.getInstance().replaceGladiator(newGladiator);
 		}
@@ -29,11 +24,8 @@ public class GeneticAlgorithm {
 	}
 	
 	public Gladiator [] collectionToGladiatorArray(Collection<GladiatorSend> gladiatorCollection) {
-//		GladiatorSend [] gladiatorSendArray = new GladiatorSend [gladiatorCollection.size()];
 		Gladiator [] gladiatorArray = new Gladiator [Gladiators.getInstance().getPopulationSize()];
-//		System.out.println("gladiatorArray: " + gladiatorArray.length);
 		int i = 0;
-//		for (int c = 0; c < gladiatorArray.length-1; c++)
 		for (GladiatorSend gladiatorSend : gladiatorCollection) {
 			gladiatorArray[i] = new Gladiator(new Chromosome(0));
 			gladiatorArray[i].getChromosome().setAge(gladiatorSend.getAge());
@@ -46,17 +38,4 @@ public class GeneticAlgorithm {
 		}
 		return gladiatorArray;
 	}
-	
-//	private Gladiator [] getGladiatorArray(GladiatorSend [] gladiatorSendArray) {
-//		Gladiator [] gladiatorArray = new Gladiator [gladiatorSendArray.length];
-//		for (int i = 0; i < gladiatorSendArray.length-1; i++) {
-//			gladiatorArray[i].getChromosome().setAge(gladiatorSendArray[i].getAge());
-//			gladiatorArray[i].getChromosome().setHealth(gladiatorSendArray[i].getHealth());
-//			gladiatorArray[i].getChromosome().setResistanceLowerBody(gladiatorSendArray[i].getResistanceLowerBody());
-//			gladiatorArray[i].getChromosome().setResistanceUpperBody(gladiatorSendArray[i].getResistanceUpperBody());
-//			gladiatorArray[i].getChromosome().setDodgeChance(gladiatorSendArray[i].getDodgeChance());
-//		}
-//		return gladiatorArray;
-//	}
-
 }
--- src/ce/itcr/gladiators/genetic/QuickSort.java
+++ src/ce/itcr/gladiators/genetic/QuickSort.java
@@ -6,12 +6,6 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-	public static void main(String[] args) {
-//		int[] arr = {4, 5, 1, 2, 3, 3};
-//		quickSort(arr, 0, arr.length-1);
-//		System.out.println(Arrays.toString(arr));
-	}
-
 	public static void quickSort(Gladiator[] arr, int start, int end){
 
 		int partition = partition(arr, start, end);
@@ -28,10 +22,6 @@ public class QuickSort {
 		Gladiator pivot = arr[end];
 
 		for(int i=start; i<end; i++){
-//			System.out.println("arr[i]" + arr[i]);
-//			System.out.println(arr[i].getFitness());
-//			System.out.println("arr[end]" + arr[end]);
-//			System.out.println(arr[end].getFitness());
 			if(arr[i].getFitness()<pivot.getFitness()){
 				Gladiator temp= arr[start];
 				arr[start]=arr[i];
